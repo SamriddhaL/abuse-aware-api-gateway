061f3a7b9344daeb5d1194f2be26cafc6d2b1796
SamriddhaL <86090897+SamriddhaL@users.noreply.github.com>
2026-02-12 20:46:21 +0545

    feat: add refresh endpoint

--- gateway/src/auth/auth.controller.ts
+++ gateway/src/auth/auth.controller.ts
@@ -7,21 +7,33 @@ import { RolesGuard } from './roles.guard';
 @Controller('auth')
 export class AuthController {
 
-    constructor(private readonly Auth: Auth){}
+    constructor(private readonly Auth: Auth) { }
     @Post('login')
-    login(@Body() user : {username: string, password: string} ){
+    login(@Body() user: { username: string, password: string }) {
         const validateUser = this.Auth.validateUser(user)
-        if (!validateUser){
+        if (!validateUser) {
             throw new UnauthorizedException('User not Found')
         }
-        const token = this.Auth.login({username: user.username})
+        const token = this.Auth.login({ username: user.username })
         return token
     }
 
     @Roles('admin')
     @UseGuards(JwtGuard, RolesGuard)
     @Get('test')
-    test(){
-        return 'valid' 
+    test() {
+        return 'valid'
     }
+
+    @Post('refresh')
+    async refresh(@Body('refreshToken') refreshToken: string) {
+        const tokens = await this.Auth.refresh(refreshToken);
+
+        if (!tokens) {
+            throw new UnauthorizedException('Invalid refresh token');
+        }
+
+        return tokens;
+    }
+
 }
